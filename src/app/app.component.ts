7f67cf1a601a877e4ba3d47b20b0d0c42dc78779
User <stu2101261018@uni-plovdiv.bg>
2024-04-24 15:17:50 +0300

    commit of the first homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'hello-world-2101261018';
+  price = "";
+  youtubelink = "";
+  youtubeInfo = "";
+  hobbies = ""
+  aboutInfo = ""
+  showInformation(index: number){      
+    this.price = ""
+    this.youtubeInfo = ""
+    this.youtubelink = ""
+    this.aboutInfo = ""
+    this.hobbies = ""    
+    switch(index){
+      case 0: this.aboutInfo = "I am me and that's all you need to know about me!"; break;
+      case 1: this.hobbies = "My hobbies are playing games and habing fun!"; break;
+      case 2: this.youtubelink = "SchaffrillasProductions"; break;
+      case 3: this.youtubeInfo = "It is a movie review channel"; break;
+      case 4: this.price = "I will cost you the measily 200$"; break;
+      default: break;
+    }
+  }
 }
